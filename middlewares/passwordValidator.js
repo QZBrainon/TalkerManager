49d99293564466d6d2b01799617af7a6c5248fe8
Brainon Queiroz <brainonqueiroz@gmail.com>
2022-08-27 19:50:22 -0300

    Req 4 TEST OK

--- middlewares/passwordValidator.js
+++ middlewares/passwordValidator.js
@@ -2,10 +2,10 @@ const passwordValidator = (req, res, next) => {
     const { password } = req.body;
     const passwordRegex = /\d{6}/;
     if (!password || password.length === 0) {
-        return res.status(400).json({ message: 'O campo password é obrigatório' });
+        return res.status(400).json({ message: 'O campo "password" é obrigatório' });
     }
     if (!passwordRegex.test(password)) {
-        return res.status(400).json({ message: 'O password deve ter pelo menos 6 caracteres' });
+        return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
     }
     next();
 };
