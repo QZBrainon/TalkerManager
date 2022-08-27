b3eabe2bfd458eb04a7cbff98f15fe62f29edf8b
Brainon Queiroz <brainonqueiroz@gmail.com>
2022-08-27 19:45:56 -0300

    req 4 adjusted

--- middlewares/emailValidator.js
+++ middlewares/emailValidator.js
@@ -2,7 +2,7 @@ const validateEmail = (req, res, next) => {
     const emailRegex = /^([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,3})$/;
     const { email } = req.body;
     if (!email || email.length === 0) {
-        return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+        return res.status(400).json({ message: 'O campo email é obrigatório' });
     }
     if (!emailRegex.test(email)) {
         return res.status(400).json({ message: 'O email deve ter o formato email@email.com' });
